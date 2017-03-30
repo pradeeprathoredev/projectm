6c61718fc3c95c4b1fe491c6d1709bbb002df5dc
pradeeprathoredev <pradeeprathoredev@gmail.com>
2017-03-30 21:41:27 +0530

    add datepicker in registration form

--- app/src/main/java/com/example/ravi/healthcare/RegisterActivity.java
+++ app/src/main/java/com/example/ravi/healthcare/RegisterActivity.java
@@ -15,7 +15,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-    private DatePicker datePicker;
+    DatePickerDialog datePickerDialog;
     private Calendar calendar;
     EditText date,name,mobile;
     Button register;
@@ -31,45 +31,37 @@ public class RegisterActivity extends AppCompatActivity {
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH);
         day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate(year, month+1, day);
+        date = (EditText) findViewById(R.id.editText2);
 
 
-    }
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(RegisterActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
 
-    @SuppressWarnings("deprecation")
-    public void setDate(View view) {
-        showDialog(999);
-        Toast.makeText(getApplicationContext(), "ca",
-                Toast.LENGTH_SHORT)
-                .show();
-    }
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
 
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        // TODO Auto-generated method stub
-        if (id == 999) {
-            return new DatePickerDialog(this,
-                    myDateListener, year, month, day);
-        }
-        return null;
-    }
-
-    private DatePickerDialog.OnDateSetListener myDateListener = new
-            DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker arg0,
-                                      int arg1, int arg2, int arg3) {
-                    // TODO Auto-generated method stub
-                    // arg1 = year
-                    // arg2 = month
-                    // arg3 = day
-                    showDate(arg1, arg2+1, arg3);
-                }
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
             };
 
-    private void showDate(int year, int month, int day) {
-        date.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
+        });
+
+
     }
+
 
 }
