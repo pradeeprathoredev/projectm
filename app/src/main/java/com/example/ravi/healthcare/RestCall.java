14365751129438b4782aa149e1634e5b9d07812a
Your Name <you@example.com>
2017-04-01 02:48:17 +0530

    Add loder

--- app/src/main/java/com/example/ravi/healthcare/RestCall.java
+++ app/src/main/java/com/example/ravi/healthcare/RestCall.java
@@ -1,5 +1,6 @@
 package com.example.ravi.healthcare;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -13,25 +14,42 @@ import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class RestCall extends AsyncHttpClient{
     private static final String BASE_URL = "http://192.168.0.114:8081/patient-web/";
-    ;
+Context context;
     private static AsyncHttpClient client = new AsyncHttpClient();
 
-    public static void get(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
+    public static void get(String url,Context context, RequestParams params, JsonHttpResponseHandler responseHandler) {
 
+        startLoder(context);
         client.get(getAbsoluteUrl(url), params, responseHandler);
+        stopLoder(startLoder(context));
     }
 
     public static void post(Context context, String url, String data, JsonHttpResponseHandler responseHandler) {
         StringEntity entity= null;
         try {
+            startLoder(context);
             entity = new StringEntity(data);
             client.post(context,getAbsoluteUrl(url),entity,"application/json", responseHandler);
+            stopLoder(startLoder(context));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
+        }
+        finally {
+            stopLoder(startLoder(context));
         }
 
     }
 
+    private static  ProgressDialog startLoder(Context context){
+        ProgressDialog pdLoading=new ProgressDialog(context);
+        pdLoading.setMessage("\tLoading...");
+        pdLoading.setCancelable(false);
+        pdLoading.show();
+        return pdLoading;
+    }
+    private static  void stopLoder(ProgressDialog pdLoading){
+        pdLoading.dismiss();
+    }
     private static String getAbsoluteUrl(String relativeUrl) {
         return BASE_URL + relativeUrl;
     }
